622033616d3d4b6e50375ef955285c5dd0b6d25d
AungSannOoDe <theflash1321@gmail.com>
2026-03-26 20:02:22 +0630

    fix:seed error-1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+// Controllers
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\CategoryController;
@@ -12,59 +15,116 @@ use App\Http\Controllers\ShopController;
 use App\Http\Controllers\SourceAreaController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UnitController;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', function () {
-    //return view('welcome');
     return view('userole');
 });
+
+Route::get('/role', fn () => view('userole'));
+
+Route::post('/role', function () {
+    return request()->role == 1
+        ? redirect()->route('user.login')
+        : redirect()->route('admin.login');
+})->name('role');
+
+/*
+|--------------------------------------------------------------------------
+| Auth Routes
+|--------------------------------------------------------------------------
+*/
 
 Auth::routes();
 
-Route::get('/user/login', [LoginController::class, 'userLoginForm'])->name('user.login')->middleware('guest');
-Route::get('/admin/login', [LoginController::class, 'adminLoginForm'])->name('admin.login')->middleware('guest');
+Route::middleware('guest')->group(function () {
+    Route::get('/user/login', [LoginController::class, 'userLoginForm'])->name('user.login');
+    Route::get('/admin/login', [LoginController::class, 'adminLoginForm'])->name('admin.login');
 
-Route::get('/role', function() {
-    return view('userole');
+    Route::get('/user/register', [RegisterController::class, 'registerForm'])->name('user.register');
 });
-Route::post('/role', function() {
-    $role=request()->role;
-    if($role == 1){
-        return redirect()->route('user.login');
-    }else{
-        return redirect()->route('admin.login');
-    }
-})->name('role');
 
-Route::get('/user/register', [RegisterController::class, 'registerForm'])->name('user.register');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
-Route::get('/order/add', [OrderController::class, 'orderForm']);
-Route::post('/order/add', [OrderController::class, 'create']);
-Route::get('/user/{id}/orders', [OrderController::class, 'show']);
-Route::get('/orders', [OrderController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| Authenticated Routes
+|--------------------------------------------------------------------------
+*/
 
-Route::get('/facts/add', [FactController::class, 'showFactForms']);
-Route::post('/category/add', [CategoryController::class, 'store']);
-Route::post('/product/add', [ProductController::class, 'store']);
-Route::post('/sourceArea/add', [SourceAreaController::class, 'store']);
-Route::post('/gate/add', [GateController::class, 'store']);
-Route::post('/shop/add', [ShopController::class, 'store']);
-Route::post('/unit/add', [UnitController::class, 'store']);
-Route::post('/orders/update/{order}', [OrderController::class, 'edit'])->name('orders.update');
-Route::get('orders/edit/{order}', [OrderController::class, 'edit'])->name('orders.edit');
-Route::post('/orders/edit/{order}', [OrderController::class, 'update'])->name('orders.edit');
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/sourceareas', [SourceAreaController::class, 'index']);
-Route::get('/gates', [GateController::class, 'index']);
-Route::get('/shops', [ShopController::class, 'index']);
-Route::get('/units', [UnitController::class, 'index']);
-Route::post('/facts/update/{id}', [FactController::class, 'edit'])->name('facts.update');
-Route::delete('/facts/delete/{id}', [FactController::class, 'delete'])->name('facts.delete');
-Route::get('/change-password', [UserController::class, 'showChangePasswordForm']);
-Route::post('/change-password', [UserController::class, 'changePassword'])->name('password.update');
-Route::get('/forgot-password', [UserController::class, 'showForgotPasswordForm'])->name('user.forgot-password');
-Route::post('/forgot-password', [UserController::class, 'forgotPassword'])->name('user.forgot-password');
-Route::get('/reset-password', [UserController::class, 'showResetPasswordForm']);
-Route::post('/reset-password', [UserController::class, 'resetPassword'])->name('user.reset-password');
-Route::post('/orders/export', [OrderController::class, 'exportAll'])->name('orders.export');
+Route::middleware('auth')->group(function () {
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Orders
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+
+    Route::get('/orders/create', [OrderController::class, 'orderForm'])->name('orders.create');
+    Route::post('/orders', [OrderController::class, 'create'])->name('orders.store');
+
+    Route::get('/orders/{order}/edit', [OrderController::class, 'edit'])->name('orders.edit');
+    Route::post('/orders/{order}', [OrderController::class, 'update'])->name('orders.update');
+
+    Route::get('/user/{id}/orders', [OrderController::class, 'show'])->name('orders.user');
+
+    Route::post('/orders/export', [OrderController::class, 'exportAll'])->name('orders.export');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Facts
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/facts/create', [FactController::class, 'showFactForms'])->name('facts.create');
+    Route::post('/facts/{id}', [FactController::class, 'edit'])->name('facts.update');
+    Route::delete('/facts/{id}', [FactController::class, 'delete'])->name('facts.delete');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Master Data (Category, Product, etc.)
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
+    Route::get('/sourceareas', [SourceAreaController::class, 'index'])->name('sourceareas.index');
+    Route::post('/sourceareas', [SourceAreaController::class, 'store'])->name('sourceareas.store');
+
+    Route::get('/gates', [GateController::class, 'index'])->name('gates.index');
+    Route::post('/gates', [GateController::class, 'store'])->name('gates.store');
+
+    Route::get('/shops', [ShopController::class, 'index'])->name('shops.index');
+    Route::post('/shops', [ShopController::class, 'store'])->name('shops.store');
+
+    Route::get('/units', [UnitController::class, 'index'])->name('units.index');
+    Route::post('/units', [UnitController::class, 'store'])->name('units.store');
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Settings
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/change-password', [UserController::class, 'showChangePasswordForm'])->name('password.form');
+    Route::post('/change-password', [UserController::class, 'changePassword'])->name('password.update');
+
+    Route::get('/forgot-password', [UserController::class, 'showForgotPasswordForm'])->name('user.forgot-password');
+    Route::post('/forgot-password', [UserController::class, 'forgotPassword']);
+
+    Route::get('/reset-password', [UserController::class, 'showResetPasswordForm'])->name('user.reset-password.form');
+    Route::post('/reset-password', [UserController::class, 'resetPassword'])->name('user.reset-password');
+
+});
