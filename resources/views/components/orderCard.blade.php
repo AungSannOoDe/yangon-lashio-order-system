6375c6de3fe00afbe31e51f14ce833dd4b42e326
AungSannOoDe <theflash1321@gmail.com>
2026-03-30 19:16:04 +0630

    feat:add edit admin  button

--- resources/views/components/orderCard.blade.php
+++ resources/views/components/orderCard.blade.php
@@ -84,6 +84,11 @@
 
             {{-- Role 2: Delete --}}
             @if(auth()->user()->role_id == 2)
+             <a href="{{ route('orders.edit', $order->id) }}" class="p-2 text-slate-300 hover:text-indigo-600 hover:bg-indigo-50 rounded-lg transition">
+                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
+                </svg>
+            </a>
             <x-deleteDialog :item="$order" :type="'order'" />
             @endif
         </div>
